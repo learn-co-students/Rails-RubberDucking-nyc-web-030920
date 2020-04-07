749d8ea2cd4cc69845b4d21bf62f81997700b043
JohnsonNYC <jkow95@gmail.com>
2020-04-07 17:42:55 -0400

    Done.

--- app/controllers/ducks_controller.rb
+++ app/controllers/ducks_controller.rb
@@ -4,4 +4,35 @@ class DucksController < ApplicationController
     @ducks = Duck.all
   end
 
+  def show
+    @duck = Duck.find(params[:id])
+  end
+
+  def new
+    @duck = Duck.new
+  end
+  
+  def create
+    @duck = Duck.new(duck_params)
+    @duck.save 
+
+    redirect_to duck_path(@duck)
+  end
+
+  def edit
+    @duck = Duck.find(params[:id])
+  end
+
+  def update
+    @duck = Duck.find(params[:id])
+    @duck.update(duck_params)
+    redirect_to duck_path(@duck)
+  end 
+
+  private 
+
+  def duck_params 
+    params.require(:duck).permit(:name,:description, :student_id)
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-10.times do
-  Student.create(name: Faker::Name.name, mod: rand(1..5))
-end
+# 10.times do
+#   Student.create(name: Faker::Name.name, mod: rand(1..5))
+# end
 
-5.times do
-  Duck.create(name: Faker::Dog.meme_phrase, description: Faker::Dog.breed)
-end
+# 5.times do
+#   Duck.create(name: Faker::Dog.meme_phrase, description: Faker::Dog.breed)
+# end
+
+
+student1= Student.create!(name:"Johnson", mod: 2)
+student1.ducks.create!(name: "ducky", description: "Lorem Ipsum")
+student1.ducks.create!(name: "Elvis", description: "Lorem Ipsum")
+
+student2= Student.create!(name:"Alexander", mod: 5)
+student2.ducks.create!(name: "Donald", description: "Random Filler Words")
+student2.ducks.create!(name: "Reggie", description: "To Fill this stuff")
+
+

03b630c29e3c3a94959829aec1bf6bab931bcfad
Yahyagok <ygok17@hotmail.com>
2020-04-08 00:27:40 -0400

    I made it

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,60 @@
 class StudentsController < ApplicationController
+  before_action :find_student, only: [:show, :edit, :update, :destroy ] 
 
   def index
     @students = Student.all
   end
+
+  def show
+
+  end
+  
+  def new 
+    @student = Student.new 
+  end 
+
+  def edit 
+
+  end
+  
+  def update
+   
+   
+    if  @student.update(student_params) 
+        redirect_to @student 
+    else
+       flash[:errors] = @student.errors.full_messages
+       redirect_to edit_student_path(@student)
+    end 
+  end 
+
+  def create 
+    @student = Student.create(student_params)
+    if @student.valid?
+      redirect_to @student 
+    else
+       flash[:errors] = @student.errors.messages
+       redirect_to new_student_path 
+    end 
+  end 
+
+  def destroy 
+    @student.destroy 
+    redirect_to students_path
+  end 
+
+  private 
+
+  def student_params 
+    params.require(:student).permit(:name, :mod) 
+  end 
+
+  def find_student 
+    @student = Student.find(params[:id])
+  end 
+
+
+
+
 
 end
